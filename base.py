1a3b06dce3350a55e321b0a0aa112bf86f8af6a6
yagmursato <yagmur_sato@windowslive.com>
2018-07-07 13:16:09 +0300

    first commit

--- base.py
+++ base.py
@@ -20,6 +20,7 @@ class NaiveBayes():
                     self.__count.append(self.__index)
                     self.__index += 1
         self.badLenght = self.__index
+        print("Kötü yorum sayısı: " , self.badLenght)
 
         with open(good_file+".txt", "r") as f:
             for line in f:
@@ -28,6 +29,8 @@ class NaiveBayes():
                     self.__count.append(self.__index)
                     self.__index += 1
         self.goodLenght = self.__index - self.badLenght
+        print("İyi yorum sayısı: ", self.goodLenght)
+        print("Toplam yorum sayısı: ", self.badLenght+self.goodLenght)
 
         return self.__bad_comments + self.__good_comments
 
@@ -38,6 +41,8 @@ class NaiveBayes():
 
         self.data = {'text': self.text,'status': self.__count}
         self.frame = pandas.DataFrame(self.data)
+        print("Datalarımız: ", self.data , "\n")
+        print("Frame haline dönmüş hali: \n" , self.frame , "\n")
 
         return self.frame["text"], self.frame["status"]
 
@@ -45,7 +50,8 @@ class NaiveBayes():
         self.vect = TfidfVectorizer(min_df=1)
         self.frame_x, self.frame_y = self.__dataFrame()
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.frame_x, self.frame_y, test_size=0.2, random_state=4)
-        self.x_trainvect = self.vect.fit_transform(self.x_train)  # öğretitğimiz yer
+        self.x_trainvect = self.vect.fit_transform(self.x_train)  # öğrettiğimiz yer
+        print("Yorumların train edilmesi\n", self.x_trainvect, "\n")
 
     def __NaiveBayes(self):
         self.__train()
@@ -53,19 +59,23 @@ class NaiveBayes():
         self.y_train = self.y_train.astype('int')
         self.mnb.fit(self.x_trainvect, self.y_train)
 
-
     def test(self, sentence):
         self.__NaiveBayes()
         x_testvect = self.vect.transform([sentence])
         pred = self.mnb.predict(x_testvect)
+        print("Yorum: " , sentence)
+        print("Benzetildiği yorum: " , pred[0])
+
+
         if pred[0] <= self.badLenght:
-            return "Bad comment"
+            return "Sonuç : Kötü yorum"
         else:
-            return "Good comment"
+            return "Sonuç: İyi yorum"
 
 
-a = NaiveBayes()
-print(a.test("çok iyi mükemmel muhteşem bir haber"))
+nb = NaiveBayes()
+comment = "İğrençsiniz"
+print(nb.test(comment))
 
 
 
